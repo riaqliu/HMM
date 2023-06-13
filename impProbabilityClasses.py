64af6bcb4d844574c387f2d17dee59bef6d546b2
riaqliu <riaqliu@gmail.com>
2023-06-13 21:30:11 +0800

    removed unused

--- impProbabilityClasses.py
+++ impProbabilityClasses.py
@@ -8,13 +8,6 @@ class PVector:
         self.states = sorted(probabilities)
         self.values = np.array(list(map(lambda x: 
             probabilities[x], self.states))).reshape(1, -1)
-        
-    @classmethod
-    def initialize(cls, states: list):
-        size = len(states)
-        rand = np.random.rand(size) / (size**2) + 1 / size
-        rand /= rand.sum(axis=0)
-        return cls(dict(zip(states, rand)))
 
     def __getitem__(self, state: str) -> float:
         return float(self.values[0, self.states.index(state)])
@@ -28,15 +21,6 @@ class PMatrix:
         self.values = np.stack([pVectors[x].values
                                 for x in self.states
                                 ]).squeeze()
-
-    @classmethod
-    def initialize(cls, states:list, observables: list):
-        size = len(states)
-        rand = np.random.rand(size, len(observables)) / (size**2) + 1 / size
-        rand /= rand.sum(axis=1).reshape(-1,1)
-        aggr = [dict(zip(observables, rand[i, :])) for i in range(size)]
-        pvec = [PVector(x) for x in aggr]
-        return cls(dict(zip(states, pvec)))
     
     @property
     def df(self):
--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from impHiddenMarkov import HiddenMarkovModel
 
 
 def score(hmm:HiddenMarkovModel, observations:list[str]) -> None:
-    print(f"Probability of the observation {observations} is \
-           {hmm.score(observations)}.")    
+    print(f"Probability of the observation {observations} is {hmm.score(observations):0.5f}.")    
+
+def decode(hmm:HiddenMarkovModel, observations:list[str]) -> None:
+    probability, weather = hmm.decode(observations)
+    print(f"The weather was most likely {weather} with {probability*100.0:.5f}% probability")
 
 
 # Hidden States
@@ -25,30 +28,26 @@ Observable = PMatrix({'Rainy': r2, 'Sunny': s2})
 
 hmm = HiddenMarkovModel(Hidden, Observable, start)
 
-# # Probability that the first observation is 'Walk'
-# score(hmm,['Walk']) 
+# Probability that the first observation is 'Walk'
+score(hmm,['Walk']) 
 
-# # Probability that the first observation is 'Shop'
-# score(hmm,['Shop']) 
+# Probability that the first observation is 'Shop'
+score(hmm,['Shop']) 
 
-# # Probability that the first observation is 'Clean'
-# score(hmm,['Clean']) 
+# Probability that the first observation is 'Clean'
+score(hmm,['Clean']) 
 
-# # Probability that the sequence 'Clean', 'Clean', 'Clean' occurs
-# score(hmm,['Clean','Clean','Clean']) 
+# Probability that the sequence 'Clean', 'Clean', 'Clean' occurs
+score(hmm,['Clean','Clean','Clean']) 
 
 # Given the observation 'Shop', 'Clean', 'Walk', find the most likely weather
-probability, weather = hmm.decode(['Shop','Clean','Walk'])
-print(f"The weather was most likely {weather} with {probability*100.0:.5f}% probability")
+decode(hmm,['Shop','Clean','Walk'])
 
 # Given observation 'Clean', 'Clean', 'Clean', find the most likely weather
-probability, weather = hmm.decode(['Clean','Clean','Clean'])
-print(f"The weather was most likely {weather} with {probability*100.0:.5f}% probability")
+decode(hmm,['Clean','Clean','Clean'])
 
 # Given observation 'Clean', 'Clean', 'Clean', find the most likely weather
-probability, weather = hmm.decode(['Shop','Shop','Walk'])
-print(f"The weather was most likely {weather} with {probability*100.0:.5f}% probability")
+decode(hmm,['Shop','Shop','Walk'])
 
 # Given observation 'Clean', 'Clean', 'Clean', find the most likely weather
-probability, weather = hmm.decode(['Walk','Clean','Walk'])
-print(f"The weather was most likely {weather} with {probability*100.0:.5f}% probability")
+decode(hmm,['Walk','Clean','Walk'])
